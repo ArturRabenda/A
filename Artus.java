755c88e7dd6e722164bf571c22abfa1687da9d61
Artur Rabenda <rabenda27178@student.pwsztar.edu.pl>
2017-02-25 15:32:22 +0100

    miska

---
+++ Artus.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arni;
+
+/**
+ *
+ * @author Artur
+ */
+public class Artus {
+    private int wiek;
+}
